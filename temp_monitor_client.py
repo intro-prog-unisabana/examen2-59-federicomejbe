22106471be3954fa03592a1fbbda5a0a2839c3e2
Federico Mejia <federicomejbe@unisabana.edu.co>
2026-04-14 14:25:04 -0500

    trail 5

--- temp_monitor_client.py
+++ temp_monitor_client.py
@@ -17,7 +17,7 @@ def main():
     with open (name, 'r') as f:
         f.readline()
         for _ in range(n):
-            temp = float(f.readline())
+            temp = f.readline()
             monitor = temp_monitor.add_reading(monitor, temp)
     # TODO: Imprimir la racha creciente mas larga
     #       usando temp_monitor.longest_rising_streak()
